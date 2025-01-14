edcf2cdb5df627f9a934f05a83b26ac52a13ff07
Xessar <xessar.sakh@gmail.com>
2025-01-14 20:22:24 +0900

    second commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,10 +1,16 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import service.CarService;
 import service.CarServiceImpl;
-
-@Controller
-public class CarController {
-    private final CarService carService = new CarServiceImpl();
-}
+//
+//@Controller
+//public class CarController {
+//    private final CarService carService = new CarServiceImpl();
+//
+//    @GetMapping("/cars")
+//    public String getCars(@RequestParam) {}
+//}
